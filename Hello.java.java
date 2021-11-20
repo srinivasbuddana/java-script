85c57c1cd3938a18decad3f2d7aea0b21b8ce97c
srinivasbuddana <91882258+srinivasbuddana@users.noreply.github.com>
2021-11-20 14:29:12 +0530

    Update Hello.java.java

--- Hello.java.java
+++ Hello.java.java
@@ -1,8 +1,8 @@
 public class Hello{
 
-      public static void main(string args[]){
+      public static void main(String[] args){
           for(int i=1;i<=10;i++){
-                system.out.println("Hello World...."+i);
+                System.out.println("Hello World...."+i);
             }
          }
       }
